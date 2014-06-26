6430422b4b376bbc7653067fbc6abd69295a1b49
Reuben Ingber <reuben.ingber@expandtheroom.com>
2014-06-25 22:21:43 -0400

    Adds sharePopUp.js

---
+++ js/sharePopUp.js
@@ -0,0 +1,23 @@
+var POP_UP_WIDTH = 700,
+    POP_UP_HEIGHT = 500;
+
+function init() {
+
+    $(document).on('click', '.js-share-pop-up', function(e) {
+        e.preventDefault();
+
+        var $this = $(this),
+            shareURL = $this.data('shareUrl'),
+            popUpWidth = $this.data('popUpWidth'),
+            popUpHeight = $this.data('popUpHeight');
+        
+        openSharePopUp(shareURL, popUpWidth, popUpHeight);
+    });
+}
+
+function openSharePopUp(shareURL, popUpWidth, popUpHeight) {
+    popUpWidth = popUpWidth || POP_UP_WIDTH;
+    popUpHeight = popUpHeight || POP_UP_HEIGHT; 
+
+    window.open(shareURL, '', 'width='+ popUpWidth + ', height=' + popUpHeight + ', menubar=no, toolbar=no');
+ }
